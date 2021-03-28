64310875b1fd84d01809628b5103a88c320b08c2
Andras Orban <orbanandris@gmail.com>
2021-03-28 15:12:06 +0200

    Introduce SASS and routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.scss';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+import NavBar from './components/NavBar/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <Router>
+        <div className="App">
+            <NavBar />
+            <div id="page-body">
+                <Route exact path="/" component={HomePage} />
+                <Route path="/about" component={AboutPage} />
+                <Route path="/articles-list" component={ArticlesList} />
+                <Route path="/article" component={ArticlePage} />
+            </div>
+        </div>
+    </Router>
+);
 
 export default App;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+        <h1>Hello, welcome to my blog!</h1>
+        <p>
+            Ut ea aliquip qui aute do occaecat deserunt ex. Est incididunt ex
+            nisi ipsum laboris culpa. Tempor eu est occaecat voluptate aliquip
+            dolor duis pariatur nostrud tempor. Ad fugiat cupidatat non nostrud
+            enim deserunt elit irure incididunt nostrud cupidatat exercitation
+            ea quis. Pariatur occaecat commodo duis cillum incididunt amet culpa
+            pariatur Lorem ipsum officia incididunt.
+        </p>
+        <p>
+            Occaecat proident incididunt laborum aliqua adipisicing ad est
+            deserunt fugiat id deserunt ullamco irure. Aliquip qui aliqua magna
+            id nulla Lorem. Laborum cupidatat laboris reprehenderit cillum sunt
+            excepteur cupidatat et duis nisi anim. Lorem est labore sunt nisi
+            deserunt fugiat Lorem in sunt esse aliqua proident. Consectetur
+            Lorem est et reprehenderit non ullamco sint voluptate. Reprehenderit
+            dolor sunt est laboris laborum.
+        </p>
+    </>
+);
+
+export default HomePage;
