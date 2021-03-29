7d32fa183bc728d48c3b98f42b310f35f9f3a9b2
Andras Orban <orbanandris@gmail.com>
2021-03-29 08:00:07 +0200

    Connect frontend and backend by fetching /api/articles/:name

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import articleContent from '../assets/article-content';
 import ArticlesList from '../components/ArticlesList/ArticlesList';
 import NotFoundPage from './NotFoundPage';
@@ -6,6 +6,20 @@ import NotFoundPage from './NotFoundPage';
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find((article) => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({
+        upvotes: 0,
+        comments: [],
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        };
+        fetchData();
+    }, [name]);
 
     if (!article) return <NotFoundPage />;
 
@@ -16,6 +30,9 @@ const ArticlePage = ({ match }) => {
     return (
         <>
             <h1>{article.title}</h1>
+            <p
+                style={{ marginBottom: '1em' }}
+            >{`This post has been upvoted ${articleInfo.upvotes} times.`}</p>
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
